4d6886688ace7d5504cea818047b44c677067dc0
Itamar Junior <cdajuniorf@gmail.com>
2026-06-18 04:16:14 -0300

    feat: criar models Link e atualizar Model User

---
+++ app/Models/Link.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    private function move($to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $swapWith = $this->user->links()->where('sort', '=', $newOrder)->first();
+        if (!$swapWith) {
+            return;
+        }
+
+        $swapWith->fill(['sort' => $order])->save();
+        $this->fill(['sort' => $newOrder])-> save();
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
+}
