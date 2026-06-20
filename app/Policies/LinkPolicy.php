af45783692ada1b6eec3c0ff923ec2e067add8ef
Itamar Junior <cdajuniorf@gmail.com>
2026-06-20 18:21:36 -0300

    feat: adicionar política de autorização para links

---
+++ app/Policies/LinkPolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class LinkPolicy
+{
+    public function atualizar(User $user, Link $link)
+    {
+        return $link->user->is($user) ? Response::allow(): Response::deny('Você não tem permissão para acessar esse link');
+    }
+}
